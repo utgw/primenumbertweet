70beb6ee65fb15bdb8186e3a0147ac465be3b884
UTAGAWA Kiki <utagawa.kiki@gmail.com>
2014-03-24 22:34:19 +0900

    Fix some

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def isp(n):
 class TweetHandler(webapp2.RequestHandler):
   def get(self):
     global api
-    if self.request.headers.get("X-Appengine-Cron"):
-      try: num = int(re.search(r'(\d+)',list(api.user_timeline(screen_name=api.me().screen_name, count=1))[0].text).group(1)) + 1
-      except tweepy.TweepError: num = 2
-      else:
-        text = u'%d は素数'%num
-        text += u'です' if isp(num) else u'ではありません'
-        try: api.update_status(text)
-        except tweepy.TweepError: pass
+    if not self.request.headers.get("X-Appengine-Cron"): return
+    try: num = int(re.search(r'(\d+)',list(api.user_timeline(screen_name=api.me().screen_name, count=1))[0].text).group(1)) + 1
+    except tweepy.TweepError: num = 2
+    else:
+      text = u'%d は素数'%num
+      text += u'です' if isp(num) else u'ではありません'
+      try: api.update_status(text)
+      except tweepy.TweepError: pass
 
 app = webapp2.WSGIApplication([('/tweet',TweetHandler)],debug=True)
 
