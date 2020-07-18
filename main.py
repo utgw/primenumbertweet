0a5ab5546d733cdc2cb4a27b0d9ab3b2511edbd9
utgwkk <utagawakiki@gmail.com>
2020-07-19 00:18:42 +0900

    autopep8

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 # coding: utf-8
 from math import sqrt
-import webapp2, tweepy, re
+import webapp2
+import tweepy
+import re
 from key import ckey, csec, atok, asec
 
-auth = tweepy.OAuthHandler(ckey,csec)
-auth.set_access_token(atok,asec)
+auth = tweepy.OAuthHandler(ckey, csec)
+auth.set_access_token(atok, asec)
 api = tweepy.API(auth)
 
+
 def isp(n):
-  if n == 2: return True
-  for k in xrange(2,int(sqrt(n))+1):
-    if n%k == 0: return False
-  return True
+    if n == 2:
+        return True
+    for k in xrange(2, int(sqrt(n))+1):
+        if n % k == 0:
+            return False
+    return True
+
 
 class TweetHandler(webapp2.RequestHandler):
-  def get(self):
-    global api
-    if not self.request.headers.get("X-Appengine-Cron"): return
-    try: num = int(re.search(r'(\d+)',list(api.user_timeline(screen_name=api.me().screen_name, count=1))[0].text).group(1)) + 1
-    except tweepy.TweepError: num = 2
-    else:
-      text = u'%d は素数'%num
-      text += u'です' if isp(num) else u'ではありません'
-      try: api.update_status(text)
-      except tweepy.TweepError: pass
+    def get(self):
+        global api
+        if not self.request.headers.get("X-Appengine-Cron"):
+            return
+        try:
+            num = int(re.search(r'(\d+)', list(api.user_timeline(
+                screen_name=api.me().screen_name, count=1))[0].text).group(1)) + 1
+        except tweepy.TweepError:
+            num = 2
+        else:
+            text = u'%d は素数' % num
+            text += u'です' if isp(num) else u'ではありません'
+            try:
+                api.update_status(text)
+            except tweepy.TweepError:
+                pass
 
-app = webapp2.WSGIApplication([('/tweet',TweetHandler)],debug=True)
 
-if __name__ == '__main__': app.run()
+app = webapp2.WSGIApplication([('/tweet', TweetHandler)], debug=True)
+
+if __name__ == '__main__':
+    app.run()
