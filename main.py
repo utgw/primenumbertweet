50561e6409b1e022a5af1ef2c2a95950ff7da97f
utgwkk <utagawakiki@gmail.com>
2020-07-19 00:21:02 +0900

    1 is not prime

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ api = tweepy.API(auth)
 
 
 def isp(n):
+    if n <= 1:
+        return False
+
     if n == 2:
         return True
+
     for k in xrange(2, int(sqrt(n))+1):
         if n % k == 0:
             return False
+
     return True
 
 
